3f69348dd8c79b1533b8a20bf780655be7ba2c01
Murial Anindo <murial.anindo@lonett.com>
2026-03-24 10:20:30 -0400

    changes for adding js

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const headers = document.querySelectorAll('.accordion-header');
 
-  headers.forEach(header => {
-    header.addEventListener('click', () => {
-      const item = header.parentElement;
-      const currentlyActive = document.querySelector('.accordion-item.active');
-      if (currentlyActive && currentlyActive !== item) {
-        currentlyActive.classList.remove('active');
-      }
-      item.classList.toggle('active');
-    });
-  });
-});
+const form = document.getElementById('customerForm');
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+})
